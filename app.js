f974bcc6716c773b846cedc94d974b756b31cd95
Madison Snaric <snaric@usc.edu>
2019-03-19 15:15:05 -0700

    finished demo

---
+++ app.js
@@ -0,0 +1,18 @@
+let WebSocket = require('ws'); 
+
+let wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+	ws.on('message', (message) => {
+		console.log(`Received: ${message}`);
+		// ws.send(message); 
+
+		wss.clients.forEach((client) => {
+			client.send(message); 
+		}); 
+	}); 
+
+	// setTimeout(() => {
+	// 	ws.send('hello'); 
+	// }, 2000);
+});
